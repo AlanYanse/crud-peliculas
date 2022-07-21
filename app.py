4de8bc7d6fdb14c229ff6cd0c03062615431296b
ElDescartableDJ <stellavillaverde4@gmail.com>
2022-07-21 13:31:16 -0300

    ui del menú realizado

--- app.py
+++ app.py
@@ -5,26 +5,67 @@ import querys as q
 exec(open("conexion.py").read())#auto-ejecuta el fichero conexion.py
 
 #conexion=sqlite3.connect("bd1.db")
-"""
-print("insertar pelicula")
 
-titulo = input("Ingresar titulo: ")
-direccion = input("Ingresar Direccion: ")
-produccion = input("Ingresar Produccion: ")
-musica = input("Ingresar Musica: ")
-pais = input("Ingresar Pais de origen: ")
-anio = int(input("Ingresar año de estreno: "))
-genero = input("Ingresar Género: ")
-sinopsis = input("Ingresar Sinopsis: ")
+#---- Zona de funciones ----
 
-q.insert(titulo, direccion, produccion, musica, pais, anio, genero, sinopsis)
+def insertar_peli():
 
-"""
+    print("insertar pelicula")
 
-print("Listar peliculas")
+    titulo = input("Ingresar titulo: ")
+    direccion = input("Ingresar Direccion: ")
+    produccion = input("Ingresar Produccion: ")
+    musica = input("Ingresar Musica: ")
+    pais = input("Ingresar Pais de origen: ")
+    anio = int(input("Ingresar año de estreno: "))
+    genero = input("Ingresar Género: ")
+    sinopsis = input("Ingresar Sinopsis: ")
 
-q.select_all()
+    q.insert(titulo, direccion, produccion, musica, pais, anio, genero, sinopsis)
 
+
+def listar_peli():
+
+    print("Listar peliculas")
+
+    q.select_all()
+
+
+
+def print_menu():
+
+    print("====================")
+    print(" CRUD DE PELÍCULAS ")
+    print("====================")
+    print(" Elegir una opción")
+    print("")
+    print("[1] listar peliculas")
+    print("[2] add new pelicula")
+    print("[3] salir")
+    print("")
+
+
+def menu():
+
+
+    print_menu()
+    
+    opcion = input("Seleccionar opcion: ")
+
+    if opcion == "3":
+        print("finalización del programa")
+    elif opcion == "1":
+        listar_peli()
+    elif opcion == "2":
+        insertar_peli()
+    else:
+        print("opción incorrecta")
+        menu()
+
+        
+#---- Inicio del programa ----
+
+menu()
 
 
 
